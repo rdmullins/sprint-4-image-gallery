fbd63b7e79dbbe067989bfae7a3aeacf70cc00f5
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-14 17:51:25 +0000

    Added for loop to populate images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
 const photos = [
     "/img/pic1.jpg",
     "/img/pic2.jpg",
@@ -8,18 +13,14 @@ const photos = [
     "/img/pic5.jpg"    
 ];
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
-
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i=0; i<photos.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', photos[i]);
+    // newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
